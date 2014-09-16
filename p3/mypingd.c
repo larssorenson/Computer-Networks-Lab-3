b77607e07bd8d89cdfc535c2941e432f4bfc850a
lsorenso <lsorenso@purdue.edu>
2014-09-16 08:45:45 -0400

    Updated MyPing Daemon

--- p3/mypingd.c
+++ p3/mypingd.c
@@ -9,10 +9,52 @@ int main(int argc, char** argv)
 	}
 	
 	int port = numberFromString(argv[1]);
+	if (port < 49152 || port > 65535)
+	{
+		write(2, "The port must be between 49152 and 65535!\r\n", 43);
+		return -1;
+	}
+	
 	#ifdef Debug
 		printInt(port);
 	#endif
 	
+	//struct hostent *server;
+	struct sockaddr clientaddr;
+	char buffer[1024];
+	char* message = "Hello!";
+	int msglen = 7;
 	
+	socklen_t addrlen = (socklen_t)sizeof(struct sockaddr_storage);
 	
+	int udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
+	int resp;
+	while(1)
+	{
+		resp = recvfrom(udpSocket, buffer, 2014, 0, &clientaddr, &addrlen);
+		if(resp <= 0)
+		{
+			write(2, "Failed to receive message!\r\n", 28);
+			break;
+		}
+		
+		resp = connect(udpSocket, &clientaddr, addrlen);
+		
+		if(resp <= 0)
+		{
+			write(2, "Failed to connect back to the client!\r\n", 39);
+			break;
+		}
+		
+		resp = sendto(udpSocket, message, msglen, 0, &clientaddr, addrlen);
+		
+		if(resp <= 0)
+		{
+			write(2, "The message could not be sent!\r\n", 32);
+			break;
+		}
+		
+	}
+	
+	return 0;
 }
