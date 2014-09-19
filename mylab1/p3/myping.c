e4bd4511579fb88e5b84bfb0a6a57ea572c08f86
lsorenso <lsorenso@purdue.edu>
2014-09-19 18:35:06 -0400

    Removed connect syscall

--- mylab1/p3/myping.c
+++ mylab1/p3/myping.c
@@ -66,7 +66,7 @@ int main(int argc, char** argv)
 	
 	int resp;
 		
-	// Attempt to connect to the server
+	/*// Attempt to connect to the server
 	resp = connect(udpSocket, (struct sockaddr*)&serveraddr, addrlen);
 	
 	if(resp <= -1)
@@ -76,7 +76,7 @@ int main(int argc, char** argv)
 			perror("Connect");
 		#endif
 		return -1;
-	}
+	}*/
 	
 	
 	// Timestamp before we send the packet
